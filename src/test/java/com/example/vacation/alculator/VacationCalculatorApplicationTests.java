2ba1f1498bdff7247a605715a3e5dde0be781f6d
zavtema <zaviruxaartemii@gmail.com>
2026-01-21 00:00:10 +0300

    Initial Spring Boot project

---
+++ src/test/java/com/example/vacation/alculator/VacationCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vacation.alculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
